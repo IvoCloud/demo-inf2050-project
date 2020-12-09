7366824e57ad1139d59b2f9d5b700b030ab06c8d
Ivo <ab691072@ens.uqam.ca>
2020-12-09 16:55:53 -0500

    Ajout de la méthode validerLongueurTableau dans App.java et les tests associés à cet methode dans AppTest.java

--- src/main/java/inf2050/App.java
+++ src/main/java/inf2050/App.java
@@ -23,15 +23,33 @@ public class App {
      * @param args args[0] - La destination de lecture.
      * @param args args[1] - La destination de l'écriture.
      */
-    public static void main(String[] args) {
-        String nomFichierIn = args[0];
-        String nomFichierOut = args[1];
+    public static void main(String[] args)throws Exception{
+        // String nomFichierIn = args[0];
+        // String nomFichierOut = args[1];
 
-        ArrayList<Plainte> plaintes = new ArrayList<>();
-        ArrayList<Statistique> statistiques = new ArrayList<>();
+        validerLongueurTableau(args,2);
 
-        plaintes = Plainte.lirePlaintes(nomFichierIn);
-        statistiques = Statistique.creerListeStatistiques(plaintes);
-        Statistique.ecrireStatistiques(statistiques, nomFichierOut);
+
+
+        System.out.println("Next");
+
+        // ArrayList<Plainte> plaintes = new ArrayList<>();
+        // ArrayList<Statistique> statistiques = new ArrayList<>();
+
+        // plaintes = Plainte.lirePlaintes(nomFichierIn);
+        // statistiques = Statistique.creerListeStatistiques(plaintes);
+        // Statistique.ecrireStatistiques(statistiques, nomFichierOut);
+    }
+
+    /**
+     * Cet méthode verifie si la longueur d'un tableau est égal à une longueur spécifiée.
+     * @param args Le tableau dont ont verifie la longueur.
+     * @param longueur  La longueur à verifier.
+     * @throws Exception Le message d'exception jeté par la méthode.
+     */
+    static void validerLongueurTableau(String[] array, int longueur)throws Exception{
+        if(array.length != longueur){
+            throw new Exception("Longueur des args[] n'est pas 2");
+        }
     }
 }
--- src/test/java/inf2050/AppTest.java
+++ src/test/java/inf2050/AppTest.java
@@ -24,34 +24,92 @@ class AppTest {
   String fichierIn = "fichier.csv";
 
   @Test
-  void testValiderSizePlaintes() {
-    ArrayList<Plainte> plaintes = Plainte.lirePlaintes(fichierIn);
-    assertEquals(plaintes.size(),6);
+  void testZeroArgsMain(){
+    String[] args = new String[]{};
+    String err = "";
+    try {
+      App.validerLongueurTableau(args,2);
+    } catch (Exception e) {
+      err = e.getMessage();
+    }
+    assertEquals("Longueur des args[] n'est pas 2",err);
   }
 
   @Test
-  void testListeStatistiques() {
-    ArrayList<Plainte> plaintes = Plainte.lirePlaintes(fichierIn);
-    assertNotNull(Statistique.creerListeStatistiques(plaintes));
+  void testOneArgsMain(){
+    String[] args = new String[]{"fichier.txt"};
+    String err = "";
+    try {
+      App.validerLongueurTableau(args,2);
+    } catch (Exception e) {
+      err = e.getMessage();
+    }
+    assertEquals("Longueur des args[] n'est pas 2",err);
   }
 
   @Test
-  void testVerifierDetails() {
-    assertFalse(Plainte.verifierDetails("Manifestation illégale","arrondissements"));
-    assertTrue(Plainte.verifierDetails("Manifestation illégale","intervention_policiere"));
+  void testTwoArgsMain(){
+    String[] args = new String[]{"fichierIn.txt","fichierOut.txt"};
+    String err = "";
+    try {
+      App.validerLongueurTableau(args,2);
+    } catch (Exception e) {
+      err = e.getMessage();
+    }
+    assertNotEquals("Longueur des args[] n'est pas 2",err);
   }
 
   @Test
-  void testValiderLignes() {
-    ArrayList<String> erreurs = new ArrayList<String>();
-
-    erreurs = Plainte.validerLigne(new String[]{"2020-09-12","13:11","Parc Carignan","Pierrefonds-Roxboro","Bagarre"});
-    assertEquals(erreurs.size(),0);
-
-    erreurs = Plainte.validerLigne(new String[]{"2020-09-12","13:11","Parc Carignan","Laval","Bagarre"});
-    assertEquals(erreurs.size(),1);
-
-    erreurs = Plainte.validerLigne(new String[]{"2020-09-12","13:11","Parc Carignan","Laval","Bruit"});
-    assertEquals(erreurs.size(),2);
+  void testThreeArgsMain(){
+    String[] args = new String[]{"fichierIn.txt","fichierOut.txt","texte"};
+    String err = "";
+    try {
+      App.validerLongueurTableau(args,2);
+    } catch (Exception e) {
+      err = e.getMessage();
+    }
+    assertEquals("Longueur des args[] n'est pas 2",err);
   }
+
+  // @Test
+  // void testArgsNotString(){
+  //   String[] args = new String[]{"fichierIn.txt","fichierOut.txt"};
+  //   assertTrue(App.validerLongueurArgs(args));
+  // }
+
+  // @Test
+  // void testGetExceptionMsg(){
+    
+  // }
+  // @Test
+  // void testValiderSizePlaintes() {
+  //   ArrayList<Plainte> plaintes = Plainte.lirePlaintes(fichierIn);
+  //   assertEquals(plaintes.size(),6);
+  // }
+
+  // @Test
+  // void testListeStatistiques() {
+  //   ArrayList<Plainte> plaintes = Plainte.lirePlaintes(fichierIn);
+  //   assertNotNull(Statistique.creerListeStatistiques(plaintes));
+  // }
+
+  // @Test
+  // void testVerifierDetails() {
+  //   assertFalse(Plainte.verifierDetails("Manifestation illégale","arrondissements"));
+  //   assertTrue(Plainte.verifierDetails("Manifestation illégale","intervention_policiere"));
+  // }
+
+  // @Test
+  // void testValiderLignes() {
+  //   ArrayList<String> erreurs = new ArrayList<String>();
+
+  //   erreurs = Plainte.validerLigne(new String[]{"2020-09-12","13:11","Parc Carignan","Pierrefonds-Roxboro","Bagarre"});
+  //   assertEquals(erreurs.size(),0);
+
+  //   erreurs = Plainte.validerLigne(new String[]{"2020-09-12","13:11","Parc Carignan","Laval","Bagarre"});
+  //   assertEquals(erreurs.size(),1);
+
+  //   erreurs = Plainte.validerLigne(new String[]{"2020-09-12","13:11","Parc Carignan","Laval","Bruit"});
+  //   assertEquals(erreurs.size(),2);
+  // }
 }
